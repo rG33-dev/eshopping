86593fbd0406304d6f129417f1ae5fbb4fa5a5a5
rG33_ <raghavsankhyaan@gmail.com>
2026-01-05 20:45:15 +0530

    Repo impl

--- app/src/main/java/com/example/eshopping/data/repo/RepoImpl.kt
+++ app/src/main/java/com/example/eshopping/data/repo/RepoImpl.kt
@@ -1,9 +1,14 @@
 package com.example.eshopping.data.repo
 
+import coil3.Uri
 import com.example.eshopping.common.ResultState
 import com.example.eshopping.common.USER_COLLECTION
+import com.example.eshopping.domain.models.BannerDataModels
 import com.example.eshopping.domain.models.CartDataModel
+import com.example.eshopping.domain.models.CategoryDataModel
+import com.example.eshopping.domain.models.ProductDataModel
 import com.example.eshopping.domain.models.UserData
+import com.example.eshopping.domain.models.UserDataParent
 import com.example.eshopping.domain.repo.Repo
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -66,6 +71,74 @@ class RepoImpl @Inject constructor(
 
             close()
         }
+    }
+
+    override fun loginUserWithMailAndPassword(userData: UserData): Flow<ResultState<Any>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUsrById(uid: Any): Flow<ResultState<Any>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUserData(userData: UserDataParent): Flow<ResultState<Any>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun userProfileImage(uri: Uri): Flow<ResultState<Any>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCategoriesInLimited(): Flow<ResultState<List<CategoryDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getProductsInLimited(): Flow<ResultState<List<ProductDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllProducts(): Flow<ResultState<List<ProductDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getProductById(productId: String): Flow<ResultState<ProductDataModel>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addToCart(cartDataModels: CartDataModel): Flow<ResultState<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addToFav(productDataModels: ProductDataModel): Flow<ResultState<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllFav(): Flow<ResultState<List<ProductDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCart(): Flow<ResultState<List<CartDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllCategories(): Flow<ResultState<List<CategoryDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCheckout(productId: String): Flow<ResultState<ProductDataModel>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBanner(): Flow<ResultState<List<BannerDataModels>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSpecificCategoryItems(categoryName: String): Flow<ResultState<List<ProductDataModel>>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllSuggestedProducts(): Flow<ResultState<List<ProductDataModel>>> {
+        TODO("Not yet implemented")
     }
 }
 
