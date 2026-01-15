e712eecc2987bd1e6b4ec576f2b906e0e6bdad75
rG33_ <raghavsankhyaan@gmail.com>
2026-01-15 21:49:56 +0530

    starting with viewmodel

--- app/src/main/java/com/example/eshopping/presentation/screens/LoginScreenUI.kt
+++ app/src/main/java/com/example/eshopping/presentation/screens/LoginScreenUI.kt
@@ -1,2 +1,12 @@
 package com.example.eshopping.presentation.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.eshopping.presentation.viewModel.LoginScreenState
+
+@Composable
+fun  LoginScreenState(navController : NavController)
+{
+
+}
+
